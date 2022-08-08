a2c1c889254f93d97caa038335783b6df8db0b10
vadim <vadim3@mail.ru>
2022-08-08 13:43:10 +0300

    lesson1

---
+++ Lesson1/ex2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+//  какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите первое число: ");
+double n1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double n2 = Convert.ToDouble(Console.ReadLine());
+
+if (n1 > n2)
+{
+    Console.WriteLine($"первое число {n1} больше второго {n2}");
+}
+else if (n2 > n1)
+{
+    Console.WriteLine($"второе число {n2} больше первого {n1}");
+}
+else 
+{
+    Console.WriteLine("введены одинаковые числа");
+}
---
+++ Lesson1/ex4/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите первое число: ");
+double n1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double n2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите третье число: ");
+double n3 = Convert.ToDouble(Console.ReadLine());
+
+if ((n1 > n2 && n1 > n3) && (n2 != n3))
+{
+    Console.WriteLine($"максимальное число из {n1}, {n2}, {n3}  - {n1}");
+}
+else if ((n2 > n1 && n2 > n3) && (n1 != n3))
+{
+    Console.WriteLine($"максимальное число из {n1}, {n2}, {n3}  - {n2}");
+}
+else if ((n3 > n1 && n3 > n2) && (n1 != n2))
+{
+    Console.WriteLine($"максимальное число из {n1}, {n2}, {n3}  - {n3}");
+}
+else
+{
+    Console.WriteLine($"среди введеных чисел {n1}, {n2}, {n3} есть одинаковые");
+}
