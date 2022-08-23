9be32b5c1e2233f3bbee272e89ae32e4aaa7374e
vadim <vadim3@mail.ru>
2022-08-23 22:24:11 +0300

    Lesson6

---
+++ Lesson6/Ex41/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.WriteLine("Ведите количество чисел для ввода");
+int count = Convert.ToInt32(Console.ReadLine());
+
+int[] num = InputNumbers(count);
+PrintArray(num);
+int countPozitive=PozitiveCount(num);
+Console.WriteLine($"Количество положительный чисел составляет: {countPozitive}");
+
+
+
+
+int[] InputNumbers(int count) //чтение вводимых чисел ------------------------------------------
+{ 
+    Console.WriteLine($"Ведите {count} числа через [ENTER]");
+    int[] input = new int[count];
+    for (int i = 0; i < count; i++)
+    {
+        input[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return input;
+}//-------------------------------------------------------------------------------------------------
+
+
+void PrintArray(int[] array)  //вывод массива в консоль ------------------------------------------
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write($" {array[array.Length - 1]}");
+    Console.WriteLine("]");
+} //-------------------------------------------------------------------------------------------------
+
+
+int PozitiveCount(int[] array) //подсчет чисел больше нуля ------------------------------------------
+{int res=0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i]>0) res++;
+}
+return res;
+}//-------------------------------------------------------------------------------------------------
+
+
---
+++ Lesson6/Ex43/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+//  заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//  значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+
+Console.WriteLine($"Ведите 4 коэффициента уравнений (b1, k1, b2, k2) через [ENTER]");
+double[] coefficient = InputNumbers(4);
+PrintArray(coefficient);
+double[] res = PointIntersection(coefficient[0], coefficient[1], coefficient[2], coefficient[3]);
+
+Console.WriteLine($"Точка пересечения прямых:");
+PrintArray(res);
+
+
+double[] InputNumbers(int count) //чтение вводимых чисел ------------------------------------------
+{
+    double[] inputNum = new double[count];
+    for (int i = 0; i < count; i++)
+    {
+        inputNum[i] = Convert.ToDouble(Console.ReadLine());
+    }
+    return inputNum;
+}//-------------------------------------------------------------------------------------------------
+
+
+double[] PointIntersection(double b1, double k1, double b2, double k2) //точка пересечения 2х прямых -----------
+{
+   
+    double[] point = new double[2];
+    //  if (k1==k2) 
+    // {
+    //     return point = [-1,  2];
+    // }
+    point[0] = (b2 - b1) / (k1 - k2);
+    point[1] = k1 * point[0] + b1;
+    return point;
+} //-------------------------------------------------------------------------------------------------
+
+
+void PrintArray(double[] array)  //вывод массива в консоль ------------------------------------------
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write("{0:N1};  ",array[i]);
+    }
+    Console.Write("{0:N1}", array[array.Length - 1]);
+    Console.WriteLine("]");
+} //-------------------------------------------------------------------------------------------------
